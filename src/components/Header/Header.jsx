cd6f686680252f119c13a621a34ed294a6679fed
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-18 17:19:31 +0600

    added inventory and login page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div className="header-manu">
-                <a href="/shop">Shop</a>
+                <a href="/">Shop</a>
                 <a href="/cart">Cart</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/login">Login</a>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop.jsx';
+import Home from './components/Layout/Home.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+import Cart from './components/Cart/Cart.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home/>,
+    children:[
+      {
+        path:"/",
+        element:<Shop/>
+      },
+      {
+        path:"/cart",
+        element:<Cart/>,
+      },
+      {
+        path:"/inventory",
+        element:<Inventory/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
